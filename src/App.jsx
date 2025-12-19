4c18b91894be02db9e85c382ce1c5254fa7eeda2
Thamindu Dasanayake <thamindudasanayake@gmail.com>
2025-12-19 17:55:50 +0530

    Implement NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero.jsx";
 import Showcase from "./sections/Showcase.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Hero/>
             <Showcase/>
         </>
